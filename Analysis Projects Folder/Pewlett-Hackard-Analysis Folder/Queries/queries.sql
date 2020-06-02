d9cd0816e1f5f2617f4c81e2937c66128a2c03aa
Femi Adeleke <femolyn1@yahoo.co.uk>
2020-06-02 13:55:39 -0500

    Adding files to folder

---
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/queries.sql
@@ -0,0 +1,37 @@
+--TO DETERMINE ELIGIBILITY FOR RETIREMENT- OUTPUT IS A LAUNDRY LIST
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Retirement eligibility-Adjsuting the code to narrow down the search to those who were employed btw 1952 -1955 and hired btw 1985 to 1988
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+--TO CREATE A NEW TABLE named retirement_info. 
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+--TO CHECK OUR NEW TABLE
+SELECT * FROM retirement_info;
+--TO EXPORT, RIGHT CLICK NEW TABLE ,RENAME DIRECTORY AND EXPORT
